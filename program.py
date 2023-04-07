aae9c22da9ea8345c644f05d86683f9c68ae66c5
Gumdrops Fingerdingles Buttermuffins Skinny-Long-Johns IV Jr III, Valiant Duke of A&W Buddy Burgers (W. Cheese) <124379501+youngestsheldon@users.noreply.github.com>
2023-04-07 20:06:12 +0000

    b

--- program.py
+++ program.py
@@ -1,6 +1,7 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction():
+def myFunction(): ('Hello World!')
   # My Function
+  print myFunction
 return "Hello World!"
